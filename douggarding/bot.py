d9c60200a12bf094ac5f5943e27979abca6cc282
Doug <doug.garding@outlook.com>
2018-03-06 14:32:30 -0700

    Made some further progress in understanding how the praw library works

--- douggarding/bot.py
+++ douggarding/bot.py
@@ -1,8 +1,24 @@
-import praw
+import praw # Python wrapper for some reddit API
 
-reddit = praw.Reddit("BOT") # bot info comes from the praw.ini
+# can use: help(class type here) to get info about the objecect type
+# can use: print(vars(class variable here) to get variables of the class
 
-print(reddit.user.me())
+# BOT info comes from the praw.ini. Contains login info related to this bot and  account
+# Reddit object is what provides convenient access to Reddit's API
+reddit = praw.Reddit("BOT")
+
+
+inbox = reddit.inbox
+#help(inbox)
+
+for message in inbox.all():
+    # print(message.body) # message.body gets the text of the message
+    # can do: author, name, created, body,
+
+    print(message.author)
+    print(message.subject)
+    print(message.body)
+
 
 #instance of a subreddit
 subreddit = reddit.subreddit("test")
@@ -11,11 +27,20 @@ print(subreddit.display_name)
 print(subreddit.title)
 print(subreddit.description)
 
+
 #instance of a redditor
 wildSketchRedditor = reddit.redditor("AWildSketchAppeared")
 print(wildSketchRedditor.comment_karma)
 
 
+for comment in wildSketchRedditor.comments.new(limit=1):
+    print(comment.body.split('\n', 1)[0][:79])
 
+
+
+
+'''
 for submission in reddit.subreddit("learnpython").new(limit=2):
     print(submission.title)
+'''
+
