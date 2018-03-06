3eba9ebc4c48acc297b0e5e6d9de5dddba19ac82
Doug <doug.garding@outlook.com>
2018-03-06 16:46:15 -0700

    Great progress on the bot. It will successfully take a username and return the top 10 subs that they've either commented in or submitted to.

--- douggarding/bot.py
+++ douggarding/bot.py
@@ -1,46 +1,47 @@
-import praw # Python wrapper for some reddit API
+import praw # Python wrapper for reddit API
+import sys # Allows to get command-line inputs
+import operator # Allows sorting a dictionary into a tuple
 
 # can use: help(class type here) to get info about the objecect type
 # can use: print(vars(class variable here) to get variables of the class
 
-# BOT info comes from the praw.ini. Contains login info related to this bot and  account
+
 # Reddit object is what provides convenient access to Reddit's API
-reddit = praw.Reddit("BOT")
+reddit = praw.Reddit("BOT") # BOT/login info comes from the praw.ini.
+DATA_LIMIT = 100.0 # Number of data points to be gathered (max is 1000)
+redditorName = sys.argv[1]  # Get the redditor from the commandline
+redditor = reddit.redditor(redditorName)
 
 
-inbox = reddit.inbox
-#help(inbox)
+def topTenDetails ( redditor, list):
 
-for message in inbox.all():
-    # print(message.body) # message.body gets the text of the message
-    # can do: author, name, created, body,
+    # Collects a tally of the 1000 newest comments from this user
+    subreddits = dict()
+    for dataPoint in list:
 
-    print(message.author)
-    print(message.subject)
-    print(message.body)
+        subReddit = "/" + dataPoint.subreddit_name_prefixed  # This comment's sub
 
+        # If this subreddit is already in the dictionary
+        if subreddits.has_key(subReddit):
+            subreddits[subReddit] = subreddits[subReddit] + 1
 
-#instance of a subreddit
-subreddit = reddit.subreddit("test")
-# print out data related to the subreddit
-print(subreddit.display_name)
-print(subreddit.title)
-print(subreddit.description)
+        # If the subreddit isn't in the dictionary
+        else:
+            subreddits[subReddit] = 1
 
-
-#instance of a redditor
-wildSketchRedditor = reddit.redditor("AWildSketchAppeared")
-print(wildSketchRedditor.comment_karma)
-
-
-for comment in wildSketchRedditor.comments.new(limit=1):
-    print(comment.body.split('\n', 1)[0][:79])
+    # Sorts the dictionary by value into a list of tuples
+    sortedValues = sorted(subreddits.items(), key=operator.itemgetter(1), reverse=True)
+    # Print out the results
+    for pair in sortedValues[0:10]:
+        percent = (pair[1] / DATA_LIMIT) * 100
+        print(pair[0] + " " + str(pair[1]) + " - " + str(percent) + "%")
 
 
+comments = redditor.comments.new(limit=DATA_LIMIT)
+posts = redditor.submissions.new(limit=DATA_LIMIT)
+upvoted = redditor.upvoted()
 
 
-'''
-for submission in reddit.subreddit("learnpython").new(limit=2):
-    print(submission.title)
-'''
+topTenDetails(redditor, comments)
+
 
