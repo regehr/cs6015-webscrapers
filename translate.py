9e4506d093b6dbcfe5cd37d7d7ce0ace424097d4
Westley Kirkham <westleykirkham@Westleys-MacBook-Pro.local>
2018-03-06 13:43:03 -0700

    handled JSONdecoder errors and got translate to work.

--- translate.py
+++ translate.py
@@ -2,7 +2,9 @@
 # https://docs.python.org/2/library/htmlparser.html
 # supports formats ./a.out url From_lang to_lang or
 # ./a.out url to_lang
-import urllib.request, sys, re
+import urllib.request, sys, re, json
+from json import JSONDecodeError
+
 from googletrans import Translator
 from bs4 import BeautifulSoup
 # print (sys.argv[0] + ' ' + sys.argv[1] +' ' + sys.argv[2])
@@ -14,16 +16,29 @@ translator = Translator()
 if (len(sys.argv) == 4):
     with urllib.request.urlopen(sys.argv[1]) as f:
         textbox = BeautifulSoup(f.read(), 'html.parser').find().text.strip()
-        re.split(". |\*|\n", textbox)
+        box_new = re.split('[&"+\-_\'*;$=#!?.{}()<>:/\n\r]+', textbox)
+        for i in box_new:
+            if i != None:
+                try:
+                    trans_phrase = translator.translate(i, dest=sys.argv[3], src=sys.argv[2])
+                    print(trans_phrase.text)
+                except JSONDecodeError:
+                    continue
 
-        #print(translator.translate(,
-             #                      dest=sys.argv[3], src=sys.argv[2]))
 
 elif (len(sys.argv) == 3):
     with urllib.request.urlopen(sys.argv[1]) as f:
-        print(translator.translate(
-            BeautifulSoup(f.read(), 'html.parser').find().text.strip(), dest=sys.argv[2]))
+        textbox = BeautifulSoup(f.read(), 'html.parser').find().text.strip()
+        box_new = re.split('[&"+\-_\'*;$=#!?.{}()<>:/\n\r]+', textbox)
+        for i in box_new:
+            if i != None:
+                try:
+                    trans_phrase = translator.translate(i, dest=sys.argv[2])
+                    print(trans_phrase.text)
+                except JSONDecodeError:
+                    continue
 
 else:
-    print("Error, improper format. try: 'program' 'url' 'to_language' 'from_language'")
+    print("Error, improper format. try: [program] [url] [to_language] [from_language]")
+    print("or:  [program] [url] [to_language]")
 
