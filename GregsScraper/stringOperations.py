6bccbe534cf261a04825b4bdae7d636e9d96550d
Greg Colledge <skitch04@cs.utah.edu>
2018-03-06 23:45:46 -0700

    fixed some dumb errors.

---
+++ GregsScraper/stringOperations.py
@@ -0,0 +1,11 @@
+#stringOperations.py
+import string
+
+def puncRemove(myString):
+    newString = ""
+    for each in myString:
+        for punc in string.punctuation:
+            if punc == each:
+                each = ""
+        newString += each
+    return newString
